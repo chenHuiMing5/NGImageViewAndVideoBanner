5b9e0259838832d592d0cd534f7d6b9dcddd984e
ngmmxh <chenhuiming@nicomama.com>
2018-10-19 17:43:11 +0800

    -n 图片视频轮播图第一次提交

---
+++ NGImageViewAndVideoBanner/NGImageViewAndVideoBanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NGImageViewAndVideoBanner
+//
+//  Created by ngmmxh on 2018/10/19.
+//  Copyright © 2018年 ngmmxh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NGImageViewAndVideoBanner/NGImageViewAndVideoBanner/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NGImageViewAndVideoBanner
+//
+//  Created by ngmmxh on 2018/10/19.
+//  Copyright © 2018年 ngmmxh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
